fdbfbd676f0ff24ca9add71841d4814abfe7d24c
harrygg <harrygg@gmail.com>
2021-01-15 08:51:07 +0200

    Added support for nth-of-type pseudo selector

---
+++ src/HtmlAgilityPack.CssSelectors.NetCore/PseudoClassSelectors/NthOfTypePseudoClass.cs
@@ -0,0 +1,11 @@
+﻿namespace HtmlAgilityPack.CssSelectors.NetCore.PseudoClassSelectors
+{
+  [PseudoClassName("nth-of-type")]
+  internal class NthOfTypePseudoClass : PseudoClass
+  {
+    protected override bool CheckNode(HtmlNode node, string parameter)
+    {
+      return node.GetIndexOnParent() == int.Parse(parameter);
+    }
+  }
+}
